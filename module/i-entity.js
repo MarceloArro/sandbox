7db3253a5d7abb42bc4013c58605463f89c19a66
Seregras <seregras@hotmail.com>
2021-06-11 13:31:55 +0000

    Replace i-entity.js

--- module/i-entity.js
+++ module/i-entity.js
@@ -28,4 +28,39 @@ export class gItem extends Item{
 
     }
 
+    async _preCreate(createData, options, userId) {
+        await super._preCreate(createData, options, userId);
+        let image="";
+        if (this.data.img == 'icons/svg/item-bag.svg') {
+            if(this.type=="cItem"){
+                image="systems/sandbox/docs/icons/sh_citem_icon.png";
+
+
+            }
+
+            if(this.type=="sheettab"){
+                image="systems/sandbox/docs/icons/sh_tab_icon.png";
+            }
+
+            if(this.type=="group"){
+                image="systems/sandbox/docs/icons/sh_group_icon.png";
+            }
+
+            if(this.type=="panel"){
+                image="systems/sandbox/docs/icons/sh_panel_icon.png";
+            }
+
+            if(this.type=="multipanel"){
+                image="systems/sandbox/docs/icons/sh_panel_icon.png";
+            }
+
+            if(this.type=="property"){
+                image="systems/sandbox/docs/icons/sh_prop_icon.png";
+            }
+
+            if(image!="")
+                this.data.update({ img: image });
+        }
+    }
+
 }
